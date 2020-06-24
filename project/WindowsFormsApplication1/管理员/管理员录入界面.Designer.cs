7e7d1d473e49efef1e4b1ef1dbe5edb4c4a2efb6
danshafaker1 <792039974@qq.com>
2020-06-24 15:09:53 +0800

    treee

---
+++ project/WindowsFormsApplication1/管理员/管理员录入界面.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class 管理员录入界面
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(管理员录入界面));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picTree = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.picTree);
+            this.panel1.Location = new System.Drawing.Point(452, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 287);
+            this.panel1.TabIndex = 0;
+            // 
+            // picTree
+            // 
+            this.picTree.Location = new System.Drawing.Point(0, 0);
+            this.picTree.Name = "picTree";
+            this.picTree.Size = new System.Drawing.Size(194, 162);
+            this.picTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picTree.TabIndex = 1;
+            this.picTree.TabStop = false;
+            // 
+            // 管理员录入界面
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1076, 553);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "管理员录入界面";
+            this.Text = "录入界面";
+            this.Load += new System.EventHandler(this.管理员录入界面_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picTree;
+
+
+
+
+
+    }
+}
---
+++ project/WindowsFormsApplication1/管理员/管理员录入界面.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using TreeGenerator;
+using System.Xml;
+namespace WindowsFormsApplication1
+{
+    public partial class 管理员录入界面 : Form
+    {
+
+
+
+        public 管理员录入界面()
+        {
+            InitializeComponent();
+
+
+
+        }
+        private TreeBuilder myTree = null;
+        private void ShowTree()
+        {
+            picTree.Image = Image.FromStream(myTree.GenerateTree(800, -1, "1000", System.Drawing.Imaging.ImageFormat.Bmp));
+        }
+
+
+
+        private void 管理员录入界面_Load(object sender, EventArgs e)
+        {
+            TreeGenerator.TreeData.TreeDataTableDataTable dtTree = new TreeData.TreeDataTableDataTable();
+            string id, fatherid;
+            string name;
+
+            using (
+              SqlConnection con = new SqlConnection("server=localhost;database=FamliyTree;uid=sa;pwd=123456"))
+            {
+                if (con.State != ConnectionState.Open)
+                {
+                    con.Close();
+                    con.Open();
+                }
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = @" Select  PERSON_NAME,PERSON_ID,FATHER_ID from PERSON_TABLE";//查询根节点
+                SqlDataReader sdr = cmd.ExecuteReader();
+
+                while (sdr.Read())
+                {
+                    id = sdr["PERSON_ID"].ToString();
+                    fatherid = sdr["FATHER_ID"].ToString();
+                    name = sdr["PERSON_NAME"].ToString();
+                    if (fatherid == "0")
+                    {
+                        dtTree.AddTreeDataTableRow(id, "", name, "");
+                    }
+                    else
+                        dtTree.AddTreeDataTableRow(id, fatherid, name, "");
+
+                }
+
+                //instantiate the object
+                //myOrgChart = new OrgChartGenerator.OrgChart(myOrgData);
+                myTree = new TreeBuilder(dtTree);
+                ShowTree();
+            }
+        }
+
+
+        private void picTree_MouseClick(object sender, MouseEventArgs e)
+        {
+            Rectangle currentRect;
+            //determine if the mouse clicked on a box, if so, show the  node description.
+            string SelectedNode = "No Node";
+            //find the node
+            foreach (XmlNode oNode in myTree.xmlTree.SelectNodes("//Node"))
+            {
+                //iterate through all nodes until found.
+                currentRect = myTree.getRectangleFromNode(oNode);
+                if (e.X >= currentRect.Left &&
+                    e.X <= currentRect.Right &&
+                    e.Y >= currentRect.Top &&
+                    e.Y <= currentRect.Bottom)
+                {
+                    SelectedNode = oNode.Attributes["nodeDescription"].InnerText;
+                    break;
+                }
+            }
+            MessageBox.Show(SelectedNode);
+        }
+
+
+
+
+    }
+
+}
